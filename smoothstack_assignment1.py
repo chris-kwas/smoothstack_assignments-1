08e2cd07b32a4bee8da849b19d796440f5149d3d
Michael Kwasnicki <mskwasn@gmail.com>
2022-02-07 18:18:19 -0500

    Update smoothstack_assignment1.py

--- smoothstack_assignment1.py
+++ smoothstack_assignment1.py
@@ -5,7 +5,7 @@ def func(P: int, R : int, L : int) -> int:
     return (R_to_monthly_percent * P) / (1 - pow((1 + R_to_monthly_percent), -L))
     
 #P = r(V)/ 1 - (1+r)^-n
-
+#new
 if __name__ == '__main__':
     print(50+50)
     print(100 - 10)
