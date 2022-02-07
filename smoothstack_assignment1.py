8bcf19ab83b7049ffe04bd01499655acdc8c737b
Michael Kwasnicki <mskwasn@gmail.com>
2022-02-07 10:59:31 -0500

    answer to last question of assignment 1-2

--- smoothstack_assignment1.py
+++ smoothstack_assignment1.py
@@ -1,1 +1,14 @@
-print(50+50)
+def func(P: int, R : int, L : int) -> int:
+    R_to_monthly_percent = R / (12 * 100)
+    
+    return (R_to_monthly_percent * P) / (1 - pow((1 + R_to_monthly_percent), -L))
+    
+
+#P = r(V)/ 1 - (1+r)^-n
+
+
+if __name__ == '__main__':
+    print(50+50)
+    print(100 - 10)
+    print("Hello World: 10")
+    print("answer = ", func(800000,6,103))
