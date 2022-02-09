23e4137e3bd222a308b4bdb1a69f41555cc1ea64
Michael Kwasnicki <mskwasn@gmail.com>
2022-02-09 02:23:57 -0500

    Update assignment 4-2.py

--- day4_assignments/assignment 4-2.py
+++ day4_assignments/assignment 4-2.py
@@ -1,1 +1,5 @@
-
+if __name__ == '__main__':
+    #start of coding exersice 9:
+    #Write a Python program, which returns a list with 2-tuples. Each tuple consists of a the order number and the product of the price per items and the quantity. The product should be increased by 10,- € if the value of the order is smaller than 100,00 €. 
+    #Write a Python program using lambda and map
+    pass
