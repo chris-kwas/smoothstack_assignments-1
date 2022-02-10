fccbe3874969f499cf2777960fe3d6c7e7c525cd
Michael Kwasnicki <mskwasn@gmail.com>
2022-02-10 18:34:33 -0500

    Update assignment 5-1.py

--- day5_assignments/assignment 5-1.py
+++ day5_assignments/assignment 5-1.py
@@ -10,15 +10,14 @@ def BMI_classification(weight, height):
         return "obese"
 
 
-if __name__ == '__main__':
-    #Day 5 –Weekend exercise work – please use functions for this problem
-    input_amt = int(input("How many entries "))
+#Day 5 –Weekend exercise work – please use functions for this problem
+input_amt = int(input("How many entries "))
 
-    bmis = list()
-    for x in range(input_amt):
-        weight = int(input("Enter weight "))
-        height = float(input("Enter height "))
-        bmis.append(BMI_classification(weight, height))
+bmis = list()
+for x in range(input_amt):
+    weight = int(input("Enter weight "))
+    height = float(input("Enter height "))
+    bmis.append(BMI_classification(weight, height))
 
-    for bmi in bmis:
-        print(bmi, end = " ")
+for bmi in bmis:
+    print(bmi, end = " ")
