7e0ccefac04f395ad343b32b92c7dc9586c5e71e
Michael Kwasnicki <mskwasn@gmail.com>
2022-02-11 02:18:49 -0500

    Update mini_project_part1.py

--- mini_project/mini_project_part1.py
+++ mini_project/mini_project_part1.py
@@ -3,34 +3,37 @@ from posixpath import split
 import openpyxl
 #import dataclasses
 import functools
+import os
 
 def parse_file_name(file):
     #things to do for error checking
     #make sure file is right type
     #make sure file is right name
 
-    months = {'january' : 'Jan', 'february':'Feb', 'march' : 'Mar', 'april' :  'Apr', 'may' :  'May', 'june' : 'Jun', 'july' : 'Jul',
-          'august' : 'Aug', 'september' :  'Sep', 'october' : 'Oct', 'november' :  'Nov', 'december' : 'Dec'}
-    months_shorthand = {'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul',
-          'Aug', 'Sep', 'Oct', 'Nov', 'Dec'}
+    # months_shorthand = {'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul',
+    #       'Aug', 'Sep', 'Oct', 'Nov', 'Dec'}
     replacements = ('_', '.')
     file = functools.reduce(lambda s, sep: s.replace(sep, ' '), replacements, file)# study this line more for latter
-    words = file.split()
-    month_year = [None,None]
-    for word in words:
-        if contains(months,word):
-            print(months[word])
-            month_year[0] = months[word]
-        if word.isnumeric():
-            month_year[1] = word[-2] + word[-1]#get last to digits
-            print(word)
-    return month_year
+    return file.split()
+    
 
+def get_month_year(parsed):
+    months = {'january' : 'Jan', 'february':'Feb', 'march' : 'Mar', 'april' :  'Apr', 'may' :  'May', 'june' : 'Jun', 'july' : 'Jul',
+        'august' : 'Aug', 'september' :  'Sep', 'october' : 'Oct', 'november' :  'Nov', 'december' : 'Dec'}
+    month_year = [None,None]
+    for text in parsed:
+        if contains(months,text):
+            month_year[0] = months[text]
+        if text.isnumeric():
+            month_year[1] = text[-2] + text[-1]#get last to digits
+    return (month_year[0],month_year[1])
 
 if __name__ == '__main__':#get rid of this line latter
-    path = "mini_project\expedia_report_monthly_january_2018.xlsx"
-
-    print(parse_file_name(path))
+    path = "mini_project\expedia_report_monthly_january_2018.com"
+    file_name, extension = os.path.splitext(path)
+    parse = parse_file_name(file_name)
+    result = get_month_year(parse)
+    print(result,extension)
 
 # wb_obj = openpyxl.load_workbook(path)
 # sheet_obj = wb_obj.active
