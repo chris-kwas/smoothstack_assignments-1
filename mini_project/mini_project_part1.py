73bf88b14d9f8cac93e2dd7c1a9b801e0b9d6f32
Michael Kwasnicki <mskwasn@gmail.com>
2022-02-10 23:03:55 -0500

    Update mini_project_part1.py

--- mini_project/mini_project_part1.py
+++ mini_project/mini_project_part1.py
@@ -1,1 +1,7 @@
+import openpyxl
 
+wb = openpyxl.load_workbook("expedia_report_monthly_january_2018.xlsx")
+ws = wb.active
+for row in ws.iter_rows(values_only=True):
+    print(row)
+#print("C:\Users\mskwa_000\AppData\Local\Temp\Temp1_problem_statement_cloud_foundations.zip\expedia_report_monthly_january_2018.xlsx")
