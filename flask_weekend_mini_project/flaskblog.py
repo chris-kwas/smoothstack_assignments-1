252e87e93cb39da06f88e328df83e6615e05ff64
Michael Kwasnicki <mskwasn@gmail.com>
2022-03-06 11:51:46 -0500

    Update flaskblog.py

--- flask_weekend_mini_project/flaskblog.py
+++ flask_weekend_mini_project/flaskblog.py
@@ -90,11 +90,10 @@ def login():
             session['email'] = email
             password = request.form['password']
             session['password'] = password
-            #print(form.data)
+            session['remember'] = form.remember.data
             if form.remember.data:
                 remember = request.form['remember']
                 session['remember'] = remember
-
             if form.email.data == 'admin@blog.com':
                 return redirect(url_for('admin'))
             else:
@@ -108,28 +107,23 @@ def logout():
     form = Logout()
     if form.is_submitted():
         #if remeber me is check need to move sessions to long term cookies
+        if bool(session['remember']) == True:
+            pass
         session.clear()        
         return redirect(url_for('login'))
     return render_template('logout.html',form=form)
 
 
-# @app.route('/login', methods=['GET','POST'])  
-# def login():
-#         if request.method == 'POST':
-#             person = request.form['name']#needs to be same variable name as in the html 
-#             session['name'] = person 
-#             return redirect(url_for('name', name=person))
-#         else:
-#             return render_template('login.html') 
-
-
-# @app.route("/name")
-# def name():
-#     if 'name' in session:
-#         name = session['name']
-#         return f'<h1>{name}</h1>'
-#     else:
-#         return  redirect(url_for('login'))
+# @app.route('/cookie', methods=['GET','POST'])  
+# def visit():
+#     #cookies is a dictionary
+#     #.get asks for specificed key(visit-count) if key doesnt exists creates it and sets value to second value (0)
+#     visit_count=request.cookies.get('visit-count', 0)#return string
+#     message =  "This is visit number {} ".format({int(visit_count)})
+#     resp = make_response(render_template('cookie.html', amt=message))#return http respose object
+#     vscount = int(visit_count) + 1
+#     resp.set_cookie('visit-count', str(vscount))#cookie values are strings
+#     return resp
 
 
 
