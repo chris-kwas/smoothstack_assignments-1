bcc4468693ccd56cac261dfe05d47a882e6490de
Michael Kwasnicki <mskwasn@gmail.com>
2022-03-11 00:48:48 -0500

    update unit testing

--- flask_weekend_mini_project/tests/functional/test_user.py
+++ flask_weekend_mini_project/tests/functional/test_user.py
@@ -1,12 +1,12 @@
-import os
-import sys
-import inspect
-#https://stackoverflow.com/questions/714063/importing-modules-from-parent-folder
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(os.path.dirname(currentdir))
-sys.path.insert(0, parentdir) 
+# import os
+# import sys
+# import inspect
+# #https://stackoverflow.com/questions/714063/importing-modules-from-parent-folder
+# currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+# parentdir = os.path.dirname(os.path.dirname(currentdir))
+# sys.path.insert(0, parentdir) 
 
-import flaskblog
+import flask_weekend_mini_project.flaskblog as flaskblog
 #from flaskblog import create_app,Config
 #from forms import *
 #from flask_weekend_mini_project.flaskblog import Config
